f9e48762f40cf3794c557f09c8d53e7a305a9fe8
shariqanwar20 <shariq.anwar@nixorcollege.edu.pk>
2021-08-25 21:22:36 +0500

    added todolist smart contract and added its functionality on frontend

---
+++ step_todo_app_using_ganache_react_metamask/blockchainBackend/contracts/TodoList.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+  uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
+  constructor() public {
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
+
+}
