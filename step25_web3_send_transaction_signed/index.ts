220d0ce09977498aaecf72ca6a7bc6ce43952e7f
AhsenRiaz <ahsenriaz2001@gmail.com>
2021-09-03 17:07:22 +0500

    Updated Step25

--- step25_web3_send_transaction_signed/index.ts
+++ step25_web3_send_transaction_signed/index.ts
@@ -1,13 +1,35 @@
-import { SignedTransaction } from './signedTransaction';
+import { EthereumAccount } from './EthereumAccount';
+const dotenv = require("dotenv");
+dotenv.config()
 
-const tx : SignedTransaction = new SignedTransaction();
+const publicAddressAccount1: string = "ENTER_ACCOUNT1_PUBLIC_ADDRESS";
+const privateKeyAccount1 = process.env.ACCOUNT1_PRIVATE_KEY
+const publicAddressAccount2: string = "ENTER_ACCOUNT_PUBLIC2_ADDRESS";
+const privateKeyAccount2 = process.env.ACCOUNT2_PRIVATE_KEY;
+const ethers: number = 0.3;
+const gasLimit: number = 21000;
+const gasPriceGwei: number = 10;
 
+const account1: EthereumAccount = new EthereumAccount(publicAddressAccount1, privateKeyAccount1);
+const account2: EthereumAccount = new EthereumAccount(publicAddressAccount2, privateKeyAccount2);
 
-tx.getAccountBalance('publicAddress').then(response => console.log(response));
+// Account1 Balance before transaction
+account1.getAccount1Balance().then(response => console.log("Account1 Balance", response));
+// Send transaction from account1
+account1.sendSignedTransactionFromAccount1(publicAddressAccount2, ethers, gasLimit, gasPriceGwei).then((response) => {
+    console.log("Account1 Transaction Details", response);
+    // Account 1 Balance after transaction 
+    account1.getAccount1Balance().then((balance) => console.log("Account1 Balance After Transaction", balance));
+});
 
+// Account2 Balance before transaction
 
-// send signed transaction from account 1 to account 2
-// You may want to save your private key in an env file
-tx.sendSignedTransaction('publicAddressAccount1','PrivateKeyAccount1','publicAddressAccount2',0.1,21000,10).then(response => console.log(response))
+account2.getAccount1Balance().then(response => console.log("Account2 Balance", response));
+// Send transaction from account1
 
+account2.sendSignedTransactionFromAccount1(publicAddressAccount2, ethers, gasLimit, gasPriceGwei).then((response) => {
+    console.log("Account2 Transaction Details", response)
+    // Account 1 Balance after transaction 
+    account2.getAccount1Balance().then((response) => console.log("Account2 Balance After Transaction", response))
+})
 
