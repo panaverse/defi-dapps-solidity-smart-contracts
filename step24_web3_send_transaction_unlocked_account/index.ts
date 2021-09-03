0aa7d0d66469f01007791f068c736bf379d2b983
AhsenRiaz <ahsenriaz2001@gmail.com>
2021-09-03 17:10:38 +0500

    Updated Step24

--- step24_web3_send_transaction_unlocked_account/index.ts
+++ step24_web3_send_transaction_unlocked_account/index.ts
@@ -1,11 +1,31 @@
-import { Transaction } from './transaction';
+import { EthereumAccount } from './EthereumAccount';
 
-const transaction : Transaction = new Transaction();
+// Note: If you want to see Account Balance in Metamask import your accout in metamask using private key of public address
 
+const account1Address = "Enter_Account1_PublicAddress";
+const account2Address = "Enter_Account2_PublicAddress"
 
-transaction.getAccountBalance('AddPublicAddress').then(response => console.log(response));
+// Instantiate Account1 
+const account1 = new EthereumAccount(account1Address);
+// Instantiate Account2
+const account2 = new EthereumAccount(account2Address);
 
-// send 1 ether from account 1 to account 2
-transaction.sendTransaction('Account1','Account2',1).then(response => console.log(response))
+// Account1 Balance Before Transaction
+account1.getAccount1Balance().then((res => console.log("Account1Balance Before Transaction", res)));
+// Send Transaction from Account1  
+account1.sendTransactionFromAccount1(account2Address, 1).then((res) => {
+    console.log("Account1 Transaction Details", res)
+    //  get Account1 Balance After Transaction
+account1.getAccount1Balance().then((balance => console.log("Account1Balance After Transaction", balance)));
 
+});
 
+// Account1 Balance Before Transaction
+account2.getAccount2Balance().then((res => console.log("Account2Balance Before Transaction", res)));
+// Send Transaction from Account2  
+account2.sendTransactionFromAccount2(account2Address, 1).then((res) => {
+    console.log("Account2 Transaction Details", res)
+    //  get Account2 Balance After Transaction
+account2.getAccount2Balance().then((balance => console.log("Account2Balance After Transaction", balance)));
+
+});
