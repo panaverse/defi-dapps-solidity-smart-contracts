33fb39aefacc6cfcd21ff3549fafc726a27ab98b
AhsenRiaz <ahsenriaz2001@gmail.com>
2021-09-03 16:17:59 +0500

    Updated Step21

--- step21_web3_node_getbalance/EthereumAccount.ts
+++ step21_web3_node_getbalance/EthereumAccount.ts
@@ -3,21 +3,26 @@ import Web3 from 'web3';
 //https://www.typescriptlang.org/docs/handbook/2/classes.html
 export class EthereumAccount {
 
-    private web3: Web3;
-    private address: string;
-   
-    // Got an Default Account Address from Etherscan: https://etherscan.io/accounts
-    public constructor(address: string = "0xDeE6238780f98c0ca2c2C28453149bEA49a3Abc9") {
-        this.address = address;
-        this.web3 = new Web3("https://mainnet.infura.io/v3/b56ef5c27bca4880844888e6a12a16ef");
-        
-    }
+  private web3: Web3;
+  private address: string;
 
-    public getAddress(): string {
-      return this.address;
-    }
+  // Got an Default Account Address from Etherscan: https://etherscan.io/accounts
+  public constructor(address:string) {
+    this.address = address;
+    // Create a project on infura.io and copy the ropsten testnetwork endpoint and paste it here
+    this.web3 = new Web3("ENTER_INFURE_NODE_URL");
 
-    public async getBalance() {
-      return await this.web3.eth.getBalance(this.address);
-    }
   }
+
+  // returns the address
+  public getAddress(): string {
+    return this.address;
+  }
+
+  // returns the balance of the address
+  public async getBalance():Promise<string | number> {
+    return await this.web3.eth.getBalance(this.address);
+  }
+
+  
+}
--- step21_web3_node_getbalance/index.ts
+++ step21_web3_node_getbalance/index.ts
@@ -1,6 +1,8 @@
 import { EthereumAccount } from './EthereumAccount';
 
-const account : EthereumAccount = new EthereumAccount();
+const accountAddress:string = "ENTER_YOUR_ACCOUNT_PUBLIC_ADDRESS"
+
+const account : EthereumAccount = new EthereumAccount(accountAddress);
 
 account.getBalance().then(response => console.log(response));
 
