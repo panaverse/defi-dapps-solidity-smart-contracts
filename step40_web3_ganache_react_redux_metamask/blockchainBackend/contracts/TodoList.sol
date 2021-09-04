de579880ff17153b884eba02772371aeae55d800
Roomi <nailaparwin@gmail.com>
2021-09-04 21:29:03 +0500

    Add files via upload

---
+++ step40_web3_ganache_react_redux_metamask/blockchainBackend/contracts/TodoList.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+  uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
+  event TaskDeleted(
+    uint id
+  );
+
+  constructor() public {
+  }
+
+  function createTask(uint _id, string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(_id, _content, false);
+    emit TaskCreated(_id, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
+
+  function deleteTask(uint _id) public {
+    uint indexToBeDeleted;
+    // if index to be deleted is not the last index, swap position.
+    for (uint i=0; i<taskCount; i++) {
+      if (tasks[i].id == _id) {
+        indexToBeDeleted = i;
+        break;
+      }
+    }
+
+      if (indexToBeDeleted < taskCount) {
+        tasks[indexToBeDeleted] = tasks[taskCount];
+      }
+    
+    // we can now reduce the array length by 1
+    taskCount--;
+    emit TaskDeleted(_id);
+  }
+}
---
+++ step40_web3_ganache_react_redux_metamask/blockchainBackend/truffle-config.js
@@ -0,0 +1,39 @@
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * trufflesuite.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura accounts
+ * are available for free at: infura.io/register.
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
+
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+//
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
+}
