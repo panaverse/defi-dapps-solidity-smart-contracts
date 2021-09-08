1e9158a7d59d17ebfd6ae2cd181cde22e21fc0f9
Mateen Mustafa <66204034+mateen7861@users.noreply.github.com>
2021-09-09 01:52:58 +0500

    added basic code for step08

---
+++ step08_solidity_enums/contracts/LearningSolidity.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.6;
+
+contract LearningSolidity{
+    
+   enum FreshJuiceSize{ SMALL, MEDIUM, LARGE }
+   FreshJuiceSize choice;
+   FreshJuiceSize constant defaultChoice = FreshJuiceSize.MEDIUM;
+
+   function setLarge() public {
+      choice = FreshJuiceSize.LARGE;
+   }
+   function getChoice() public view returns (FreshJuiceSize) {
+      return choice;
+   }
+   function getDefaultChoice() public pure returns (uint) {
+      return uint(defaultChoice);
+   }
+    
+    
+}
