1d2d5e86397bba74ada74c5627058ea30b3b68c5
Waris Hasan <53475608+warishasan@users.noreply.github.com>
2021-09-21 13:55:27 +0500

    updated some code

--- step07_solidity_function_modifiers/contracts/LearningSolidity.sol
+++ step07_solidity_function_modifiers/contracts/LearningSolidity.sol
@@ -1,30 +1,51 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.6;
+pragma solidity >=0.7.0 <0.9.0;
 
-contract LearningSolidity{
+/**
+ * @title Owner
+ * @dev Set & change owner
+ */
+contract Owner {
+
+    address private owner;
     
-   //create a string variable.
-
-    string private name;
-
+    // event for EVM logging
+    event OwnerSet(address indexed oldOwner, address indexed newOwner);
     
-//modifier checks that the caller of the function is the owner
-    modifier onlyOwner(bool isAdmin) {
-         if(isAdmin){
-             _;
-         }
-    }
-
-//set name.  Only the admin can call because a modifier is specified
-    function setName(string memory newName,bool isAdmin) public onlyOwner(isAdmin){
-        name = newName;
-    }
-
-//get the name
-    function getName () public view returns (string memory) {
-        return name;
+    // modifier to check if caller is owner
+    modifier isOwner() {
+        // If the first argument of 'require' evaluates to 'false', execution terminates and all
+        // changes to the state and to Ether balances are reverted.
+        // This used to consume all gas in old EVM versions, but not anymore.
+        // It is often a good idea to use 'require' to check if functions are called correctly.
+        // As a second argument, you can also provide an explanation about what went wrong.
+        require(msg.sender == owner, "Caller is not owner");
+        _;
     }
     
-    
+    /**
+     * @dev Set contract deployer as owner
+     */
+    constructor() {
+        owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
+        emit OwnerSet(address(0), owner);
+    }
+
+    /**
+     * @dev Change owner
+     * @param newOwner address of new owner
+     */
+    function changeOwner(address newOwner) public isOwner {
+        emit OwnerSet(owner, newOwner);
+        owner = newOwner;
+    }
+
+    /**
+     * @dev Return owner address 
+     * @return address of owner
+     */
+    function getOwner() external view returns (address) {
+        return owner;
+    }
 }
--- step10_solidity_events/contracts/LearningSolidity.sol
+++ step10_solidity_events/contracts/LearningSolidity.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.6;
+pragma solidity >=0.7.0 <0.9.0;
+
 
 contract LearningSolidity{
    //Declare an Event
-event Deposit(address indexed _from, bytes32 indexed _id, uint _value);
+event Deposit(address indexed _from, uint _value);
 
- function deposit(bytes32 _id) public payable {      
+ function deposit() public payable {      
       
 //Emit an event
-emit Deposit(msg.sender, _id, msg.value);
+emit Deposit(msg.sender, msg.value);
    }
     
 }
+
