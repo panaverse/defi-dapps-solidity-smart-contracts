7c890d85c18ab1b6da1b9eefa0b1c9bebe9f3da8
Mateen Mustafa <66204034+mateen7861@users.noreply.github.com>
2021-09-21 16:46:12 +0500

    made some changes

--- step10_solidity_events/contracts/LearningSolidity.sol
+++ step10_solidity_events/contracts/LearningSolidity.sol
@@ -2,18 +2,15 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+contract LearningSolidity {
+    //Declare an Event
+    // event keyword is used to declare events in solidity
+    // LogMyName  event will log the name passed to it
+    event LogMyName(string name);
 
-contract LearningSolidity{
-   //Declare an Event
-event Deposit(address indexed _from, uint _value);
-
-
- // Defining a function for logging event 
- function deposit() public payable {      
-      
-//Emit an event
-emit Deposit(msg.sender, msg.value);
-   }
-    
+    // Defining a function for logging event
+    function deposit() public payable {
+        // emit keyword is used to trigger a specific event with its name in solidity
+        emit LogMyName("Zia Khan");
+    }
 }
-
--- step14_solidity_interface/contracts/LearningSolidity.sol
+++ step14_solidity_interface/contracts/LearningSolidity.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
+
+// Declare an interface
 interface Calculator {
    function getResult() external view returns(uint);
 }
+
+// Implement interface Calculator on Test contract
 contract Test is Calculator {
    function getResult() override external pure returns(uint){
       uint a = 1; 
