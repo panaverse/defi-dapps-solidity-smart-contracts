a58f794e2c996939ec20ba46ab4cbd22d663267c
Zia Khan <ziaukhan@hotmail.com>
2021-10-04 23:07:25 +0500

    added more tests

--- step04_first_coin/contracts/FirstCoin.sol
+++ step04_first_coin/contracts/FirstCoin.sol
@@ -16,11 +16,17 @@ contract FirstCoin  {
     mapping(address => uint256) private balances;
     mapping(address => mapping (address => uint256)) private allowed;
     uint256 private totalSupply_;
+    address private owner_; 
 
     constructor(uint256 total) {
         totalSupply_ = total;
         balances[msg.sender] = totalSupply_;
+        owner_ = msg.sender;
         console.log("Total supply created and assigned to Owner: ", totalSupply_);
+    }
+
+    function owner() public view returns (address){
+        return owner_;
     }
 
     function totalSupply() public view returns (uint256) {
@@ -34,7 +40,7 @@ contract FirstCoin  {
     }
 
     function transfer(address receiver, uint numTokens) public returns (bool) {
-        require(numTokens <= balances[msg.sender]);
+        require(numTokens <= balances[msg.sender], "Not enough tokens.");
         balances[msg.sender] = balances[msg.sender] - numTokens;
         balances[receiver] = balances[receiver] + numTokens;
         emit Transfer(msg.sender, receiver, numTokens);
@@ -50,19 +56,19 @@ contract FirstCoin  {
     }
 
 
-    function allowance(address owner, address delegate) public view returns (uint) {
-        console.log("The owner: '%s' has approved '%s' tokens for the delegate '%s'", owner, allowed[owner][delegate], delegate);
-        return allowed[owner][delegate];
+    function allowance(address tokenowner, address delegate) public view returns (uint) {
+        console.log("The owner: '%s' has approved '%s' tokens for the delegate '%s'", tokenowner, allowed[tokenowner][delegate], delegate);
+        return allowed[tokenowner][delegate];
     }
 
-    function transferFrom(address owner, address buyer, uint numTokens) public returns (bool) {
-        require(numTokens <= balances[owner]);
-        require(numTokens <= allowed[owner][msg.sender]);
-        balances[owner] = balances[owner] - numTokens;
-        allowed[owner][msg.sender] = allowed[owner][msg.sender] - numTokens;
+    function transferFrom(address ownerAddr, address buyer, uint numTokens) public returns (bool) {
+        require(numTokens <= balances[ownerAddr]);
+        require(numTokens <= allowed[ownerAddr][msg.sender]);
+        balances[ownerAddr] = balances[ownerAddr] - numTokens;
+        allowed[ownerAddr][msg.sender] = allowed[ownerAddr][msg.sender] - numTokens;
         balances[buyer] = balances[buyer] + numTokens;
-        emit Transfer(owner, buyer, numTokens);
-        console.log("With owners: '%s' permission delegate '%s' is transfering '%s' tokens to the buyer", owner, msg.sender, numTokens);
+        emit Transfer(ownerAddr, buyer, numTokens);
+        console.log("With owners: '%s' permission delegate '%s' is transfering '%s' tokens to the buyer", ownerAddr, msg.sender, numTokens);
         return true;
     }
 
