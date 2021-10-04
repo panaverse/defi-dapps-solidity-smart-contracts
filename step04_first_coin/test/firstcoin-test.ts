0d6e0a1954ac5fc6c480c00e9e3af7f90798d545
Zia Khan <ziaukhan@hotmail.com>
2021-10-04 11:26:08 +0500

    update test

--- step04_first_coin/test/firstcoin-test.ts
+++ step04_first_coin/test/firstcoin-test.ts
@@ -1,14 +1,19 @@
 import { ethers, waffle } from "hardhat";
 import { expect } from "chai";
+import { Address } from "cluster";
 
 
 describe("FirtCoin", function () {
   it("Should return the total coins", async function () {
+    const [owner, addr1] = await ethers.getSigners();
+
     const FirstCoin = await ethers.getContractFactory("FirstCoin");
     const firstCoin = await FirstCoin.deploy(1000);
     await firstCoin.deployed();
 
     expect(await firstCoin.totalSupply()).to.equal(1000);
+
+    expect(await firstCoin.balanceOf(await owner.getAddress())).to.equal(1000);
 
     //const setGreetingTx = await firstCoin.;
 
