e008ca9996d93742417e10aea48cc6d0a416f608
Zia Khan <ziaukhan@hotmail.com>
2021-11-01 09:55:44 +0500

    minor changes

--- step06_first_coin/hardhat.config.ts
+++ step06_first_coin/hardhat.config.ts
@@ -6,13 +6,13 @@ import '@nomiclabs/hardhat-ethers';
 
 /// Go to https://infura.io, sign up, create
 // a new App in its dashboard, and replace "KEY" with its key
-const INFURA_API_KEY = "27bdafee5f5f4f57873cc1ce58ba2801";
+const INFURA_API_KEY = "";
 
 // Replace this private key with your Ropsten account private key
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Be aware of NEVER putting real Ether into testing accounts
-const ROPSTEN_PRIVATE_KEY = "6ff5a594471e9b9e74461257e151a853d4a0bf6091072b196f4c2f6649791f19";
+const ROPSTEN_PRIVATE_KEY = "";
 
 
 // This is a sample Hardhat task. To learn how to create your own go to
