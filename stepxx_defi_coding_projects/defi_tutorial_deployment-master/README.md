4a7f5b52d86794da314d8f6e152bf67bf3ab347c
OmarMWarraich <earthling986@gmail.com>
2021-11-15 15:26:34 -0500

    Deploy a fullstack dapp on ipfs/fleek

---
+++ stepxx_defi_coding_projects/defi_tutorial_deployment-master/README.md
@@ -0,0 +1,13 @@
+RESOURCES:
+
+https://infura.io/
+
+https://fleek.co/
+
+https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en
+
+https://faucet.rinkeby.io/
+
+Deployed App:
+
+https://ipfs.fleek.co/ipfs/QmUtxBDJ3PjHEJtx37PDoZCBivne5L9Czs6d2EJfkAZP32/
---
+++ stepxx_defi_coding_projects/defi_tutorial_deployment-master/truffle-config.js
@@ -0,0 +1,37 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const privateKeys = process.env.PRIVATE_KEYS || ""
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(','), // Array of account private keys
+          `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 4
+    }
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      evmVersion: "petersburg"
+    }
+  }
+}
