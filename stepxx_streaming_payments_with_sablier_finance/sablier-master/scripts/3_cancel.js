57c0099c9b2e46cf472168defaba5467014f1a7c
OmarMWarraich <earthling986@gmail.com>
2021-11-15 12:21:56 -0500

    Streaming payments with sablier-finance

---
+++ stepxx_streaming_payments_with_sablier_finance/sablier-master/scripts/3_cancel.js
@@ -0,0 +1,5 @@
+/** !H0m3W0rk #2
+  *
+  * Write code for stream cancellation.
+  * more: https://docs.sablier.finance/streams#cancel-stream
+ */
---
+++ stepxx_streaming_payments_with_sablier_finance/sablier-master/scripts/4_compoundStream.js
@@ -0,0 +1,5 @@
+/** !H0m3W0rk #3
+  *
+  * Write code for compound stream.
+  * more: https://docs.sablier.finance/compounding-streams
+ */
