3f380b154a75b22efaf65e4d8750d2b3747007bf
Zia Khan <ziaukhan@hotmail.com>
2022-03-29 15:05:04 +0500

    extend from ERC20PresetMinterPauser

--- step06B_chap9_OpenZeppelin/contracts/MyToken.sol
+++ step06B_chap9_OpenZeppelin/contracts/MyToken.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
-contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {}
+contract MyToken is ERC20PresetMinterPauser {
+    constructor() ERC20PresetMinterPauser("MyToken", "MTK") {}
 }
